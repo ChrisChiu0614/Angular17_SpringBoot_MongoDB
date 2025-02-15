488e90ce322c40fce984dd4d180333a039f35f93
Chris Chiu <chris820614@gmail.com>
2025-02-15 10:07:29 +0800

    updata all

--- BackEnd/src/main/java/com/chris/spring/data/mongodb/repository/PagingAndSortingRepository.java
+++ BackEnd/src/main/java/com/chris/spring/data/mongodb/repository/PagingAndSortingRepository.java
@@ -1,9 +1,12 @@
 package com.chris.spring.data.mongodb.repository;
 
+import com.chris.spring.data.mongodb.model.Tutorial;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PagingAndSortingRepository<T, ID> extends CrudRepository {
     Page<T> findAll(Pageable pageable);
+    Page<Tutorial> findByPublished(boolean published, Pageable pageable);
+    Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
 }
